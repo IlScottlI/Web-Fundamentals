f119657f06fc56f6246f71ea5e7f60c956cb12fe
lIScottIl <scotteverettjohnson84@gmail.com>
2020-11-14 15:21:15 -0500

    Playing with Objects

---
+++ JavaScript-Objects Predict/response.js
@@ -0,0 +1,17 @@
+var users = [
+  {
+    name: "Michael",
+    age: 37,
+  },
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "David",
+    age: 27,
+  },
+];
+users.forEach((element) => {
+  console.log(`${element.name} - ${element.age}`);
+});
